8aae3de022603043681d2281a24e452713f0a323
gainen <morishirogainen@gmail.com>
2026-08-18 19:01:21 +0200

    combat tweaks

--- Fragmented-Serpent/Assets/Scripts/Enemy.cs
+++ Fragmented-Serpent/Assets/Scripts/Enemy.cs
@@ -4,21 +4,7 @@ public class Enemy : MonoBehaviour
 {
     public float HP = 100f;
 
-    public float attackRange = 3f;
-    public float chaseRange = 30f;
-    public float movespeed = 3f;
-
-    private Rigidbody rb;
-    private Player player;
-
-
-    public GameObject EnemyProjectilePrefab;
-    public Transform spawnpoint;
-
-    public int spawnlimit = 5;
-    public float ATKCooldown = 5f;
-    public float ATKTimer = 0f;
-
+    
 
    
 }
--- Fragmented-Serpent/Assets/Scripts/EnemyProjectile.cs
+++ Fragmented-Serpent/Assets/Scripts/EnemyProjectile.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
 
 public class EnemyProjectile : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   public float moveSpeed = 10f;
+    public float moveSpeed = 10f;
     public float damage = 10f;
     public float lifetime = 5f;
-   
+    public Player player;
 
     private Rigidbody rb;
     private void Awake()
@@ -25,5 +27,21 @@ public class EnemyProjectile : MonoBehaviour
             Destroy(gameObject);
         }
     }
-   
-}   
+    private bool isPlayerHit = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isPlayerHit) return;
+        {
+            Player player = GetComponent<Player>();
+
+            if (player != null)
+            {
+                isPlayerHit = true;
+
+                player.HP -= damage;
+            }
+        }
+        Debug.Log("isPlayer" + isPlayerHit);
+
+    }
+}
